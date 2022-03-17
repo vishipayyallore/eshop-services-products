db91b55a5dddcd774d54a30336fea0a69f5dd4a5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-03-17 09:22:15 +0530

    Initial Version of Test Projects and Code Coverage (#12)
    * Initial Version of Test Projects
    
    * Update Products-Api-CI.yml
    
    * Update Products-Api-CI.yml
    
    * Update Products-Api-CI.yml
    
    * Update Products-Api-CI.yml
    
    * Update Products-Api-CI.yml
    
    * Test Cases
    
    * Update ProductTestCases.cs

---
+++ Tests/Products.Core.Tests/Entities/ProductTestCases.cs
@@ -0,0 +1,38 @@
+using Products.Core.Entities;
+using System;
+using Xunit;
+
+namespace Products.Core.Tests
+{
+    public class ProductTestCases
+    {
+
+        [Fact]
+        public void When_New_Product_Instance_Created()
+        {
+            var product = new Product
+            {
+                Id = Guid.NewGuid().ToString(),
+
+                Name = "Some Product",
+
+                Category = "Some Category",
+
+                Summary = "Some Summary",
+
+                Description = "Some Description",
+
+                ImageFile = "Some ImageFile",
+
+                Price = 1.0m,
+
+                CreatedDateTime = DateTime.Now,
+
+                ModifiedDateTime = DateTime.Now
+            };
+        
+            Assert.NotNull(product);
+        }
+
+    }
+}
---
+++ Tests/Products.Data.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Products.Data.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
