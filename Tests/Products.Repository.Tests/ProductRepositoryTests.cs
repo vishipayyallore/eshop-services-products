040311b341d21552ec12f1df49f95be0be380a1c
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-03-21 22:02:15 +0530

    Added 3rd Test Case, also updated the Readme.md

--- Tests/Products.Repository.Tests/ProductRepositoryTests.cs
+++ Tests/Products.Repository.Tests/ProductRepositoryTests.cs
@@ -134,6 +134,43 @@ namespace Products.Repository.Tests
             Assert.Equal(_productsList[0].Id, productRetrieved.Id);
         }
 
+        [Fact]
+        public async void When_ProductRepository_GetProductsByName_IsCalled_WithValid_Name_Returns_Data()
+        {
+            List<Product> _productsList = GetDummyProducts();
+
+            // Arrange
+            asyncCursor = new Mock<IAsyncCursor<Product>>();
+            mockIMongoCollection = new Mock<IMongoCollection<Product>>();
+            mockedProductContext = new Mock<IProductContext>();
+
+            asyncCursor.SetupSequence(_async => _async.MoveNextAsync(default))
+                .Returns(Task.FromResult(true))
+                .Returns(Task.FromResult(false));
+            asyncCursor.SetupGet(_async => _async.Current).Returns(_productsList);
+
+            mockIMongoCollection.Setup(_collection => _collection.FindAsync(
+                 It.IsAny<FilterDefinition<Product>>(),
+                 It.IsAny<FindOptions<Product>>(),
+                 default))
+               .ReturnsAsync(asyncCursor.Object);
+
+            //Act 
+            mockedProductContext.SetupGet(x => x.Products).Returns(mockIMongoCollection.Object);
+
+            var productRepository = new ProductRepository(mockedProductContext.Object);
+
+#pragma warning disable CS8604 // Possible null reference argument.
+            var productRetrieved = await productRepository.GetProductsByName(_productsList[1].Name);
+#pragma warning restore CS8604 // Possible null reference argument.
+
+            //Assert 
+            Assert.NotNull(productRetrieved);
+
+            // TODO: Fix the issue. It is returning two Products
+            // Assert.Equal(_productsList[1].Name, productRetrieved1.FirstOrDefault()?.Name);
+        }
+
         private static List<Product> GetDummyProducts()
         {
             return new List<Product>()
