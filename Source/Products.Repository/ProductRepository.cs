344271da1830d8d33adc964fb195c20eab621d5c
Robert Collins <roberto.tomas.cuentas@gmail.com>
2022-03-24 10:41:13 -0400

    Working test cases on conditional matches (#23)
    * modified search
    
    * where
    
    * .
    
    * some missing improvements on second test case

--- Source/Products.Repository/ProductRepository.cs
+++ Source/Products.Repository/ProductRepository.cs
@@ -32,7 +32,7 @@ namespace Products.Repository
 
         public async Task<IEnumerable<Product>> GetProductsByName(string name)
         {
-            FilterDefinition<Product> filter = Builders<Product>.Filter.ElemMatch(p => p.Name, name);
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name, name);
 
             return await _context
                             .Products
