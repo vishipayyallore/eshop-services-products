2f97379d6b2189afe85a955f26ff00d062726e66
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-03-29 14:20:02 +0530

    Update az-acr-create.sh

--- Scripts/az-acr-create.sh
+++ Scripts/az-acr-create.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-output=$(az acr list -g rg-dnlh-12mar-dev --query '[].name')
+RESOURCEGROUPNAME=rg-dnlh-12mar-dev
+ACRNAME=acreshopdev
+
+# az acr list -g rg-dnlh-12mar-dev --query '[].name | length(@)
+
+output=$(az acr list -g rg-dnlh-12mar-dev --query '[].name | length(@)')
 echo $output
 
+if [ $output == 0 ]
+then
+    echo $ACRNAME" ACR does not exist ... Creating ACR !"
+
+    az acr create --resource-group $RESOURCEGROUPNAME --name $ACRNAME --sku Basic
+
+else
+
+    echo $ACRNAME" ACR exist!"    
+fi
