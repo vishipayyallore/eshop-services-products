0d385ba41a99f86576bc4a7ac7a802cdb8125dbd
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-04-04 17:26:24 +0530

    Update deploydockertoaci.sh

--- Scripts/deploydockertoaci.sh
+++ Scripts/deploydockertoaci.sh
@@ -3,8 +3,8 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Description: Script to create ACI single instance using docker image from docker registry
 # Author: Apaar, and Swamy
-# Date: 24-Mar-2022
-# Modified: 01-Apr-2022
+# Date: 04-Apr-2022
+# Modified: 04-Apr-2022
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 az container create  --resource-group rg-dnlh-12mar-dev --name aci-productsapiv1 \
