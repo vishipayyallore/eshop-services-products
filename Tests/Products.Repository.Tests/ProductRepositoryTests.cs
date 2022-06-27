10af87d8399e9e09c937ddc4d4fb92098b053a7d
Robert Collins <roberto.tomas.cuentas@gmail.com>
2022-06-27 13:39:32 -0400

    Create and Delete tests (#50)
    * Create and Delete tests
    
    * updated tests
    
    swamy please help, each test runs individually in debugger but the tests window for me is not running any test in this file when the first or second of the 3 are left uncommented.
    
    * migrated to pointer-based result so it can update to reflect changes.

--- Tests/Products.Repository.Tests/ProductRepositoryTests.cs
+++ Tests/Products.Repository.Tests/ProductRepositoryTests.cs
@@ -13,7 +13,6 @@ using Xunit;
 
 namespace Products.Repository.Tests
 {
-
     [ExcludeFromCodeCoverage]
     public class ProductRepositoryTests : IDisposable
     {
@@ -229,6 +228,175 @@ namespace Products.Repository.Tests
 
             Assert.Equal(_expectedResultsList.Count, received.Count());
         }
+
+
+        [Fact]
+        public async void Create_Product_And_Inserts_It()
+        {
+            List<Product> _productsList = GetDummyProducts();
+
+            // Arrange
+            mockIMongoCollection = new Mock<IMongoCollection<Product>>();
+            mockedProductContext = new Mock<IProductContext>();
+
+            //Act
+            Product? _transientProduct = null;
+            mockedProductContext.SetupGet(_context => _context.Products).Returns(mockIMongoCollection.Object);
+            mockedProductContext.Setup(_context => _context.Products.InsertOneAsync(
+                It.IsAny<Product>(),
+                It.IsAny<InsertOneOptions>(),
+                default))
+              .Callback((Product _newProduct, object _options, object _cancellation) =>
+              {
+                  _transientProduct = _newProduct;
+                  _productsList.Add(_newProduct);
+              })
+              .Returns(Task.FromResult(_transientProduct));
+            var productRepository = new ProductRepository(mockedProductContext?.Object, mockedMapper?.Object);
+
+            //precondition
+            // Assert productRepository does not contain product with name MyPhone
+            var precondition = _productsList.FindAll(x => x.Name == "MyPhone").Count();
+            Assert.Equal(0, precondition);
+
+            Product product = new Product
+            {
+                Name = "MyPhone"
+            };
+            await productRepository.CreateProduct(product);
+
+            // validation
+            // Assert productRepository does contain product with name MyPhon
+            var receivedRaw = _productsList.FindAll(x => x.Name == "MyPhone").ToList();
+            int received = receivedRaw.Count();
+            Assert.Equal(1, received);
+
+            // TODO: Verify Auto Generation on Id. -- this is not currently working
+            // var receivedProduct = receivedRaw[0];
+            // Assert.NotNull(receivedProduct.Id);
+        }
+
+
+        [Fact]
+        public async void Delete_Product_Deletes_It()
+        {
+            List<Product> _productsList = GetDummyProducts();
+            Product product = new Product
+            {
+                Id = "602d2149e773f2a3990b47f8",
+                Name = "MyPhone"
+            };
+            int expectedLength = _productsList.Count();
+
+            // Arrange
+            mockIMongoCollection = new Mock<IMongoCollection<Product>>();
+            mockedProductContext = new Mock<IProductContext>();
+
+            //Act
+            Product? _transientProduct = null;
+            bool transientDeleted = false;
+            DeleteResult? transientDeleteResult = new DeleteResult.Acknowledged(0);
+            mockedProductContext.SetupGet(_context => _context.Products).Returns(mockIMongoCollection.Object);
+            mockedProductContext.Setup(_context => _context.Products.InsertOneAsync(
+                It.IsAny<Product>(),
+                It.IsAny<InsertOneOptions>(),
+                default))
+              .Callback((Product _newProduct, object _options, object _cancellation) =>
+              {
+                  _transientProduct = _newProduct;
+                  _productsList.Add(_newProduct);
+              })
+              .Returns(Task.FromResult(_transientProduct));
+            mockedProductContext.Setup(_context => _context.Products.DeleteOneAsync(
+                It.IsAny<FilterDefinition<Product>>(),
+                default))
+              .Callback((FilterDefinition<Product> _filterProducts, object _cancellation) =>
+              {
+                  _productsList.RemoveAll(item =>
+                  {
+                      if (transientDeleted) return false;
+                      bool matches = item.Id == product.Id;
+                      if (matches)
+                      {
+                          transientDeleted = true;
+                          transientDeleteResult = new DeleteResult.Acknowledged(1);
+                          return true;
+                      }
+                      return false;
+                  });
+              })
+              .Returns(() => Task.FromResult(transientDeleteResult));
+
+            var productRepository = new ProductRepository(mockedProductContext?.Object, mockedMapper?.Object);
+
+            //precondition
+            // Assert productRepository does not contain product with our product's id
+            var precondition1 = _productsList.FindAll(x => x.Id == product.Id).Count();
+            Assert.Equal(0, precondition1);
+
+            await productRepository.CreateProduct(product);
+
+            //precondition
+            // Assert productRepository now contains product with our product's id
+            var precondition = _productsList.FindAll(x => x.Id == product.Id).Count();
+            Assert.Equal(1, precondition);
+
+            bool received = await productRepository.DeleteProduct(product.Id);
+
+            // validation
+            Assert.True(received);
+            Assert.Equal(expectedLength, _productsList.Count());
+        }
+
+
+        [Fact]
+        public async void Delete_Product_Returns_False_If_No_Product_Was_Deleted()
+        {
+            List<Product> _productsList = GetDummyProducts();
+            string targetId = "dummy-data";
+            int expectedLength = _productsList.Count();
+
+            // Arrange
+            mockIMongoCollection = new Mock<IMongoCollection<Product>>();
+            mockedProductContext = new Mock<IProductContext>();
+
+            //Act
+            bool transientDeleted = false;
+            DeleteResult? transientDeleteResult = new DeleteResult.Acknowledged(0);
+            mockedProductContext.SetupGet(_context => _context.Products).Returns(mockIMongoCollection.Object);
+            mockedProductContext.Setup(_context => _context.Products.DeleteOneAsync(
+                It.IsAny<FilterDefinition<Product>>(),
+                default))
+              .Callback((FilterDefinition<Product> _filterProducts, object _cancellation) =>
+              {
+                  _productsList.RemoveAll(item => {
+                      if (transientDeleted) return false;
+                      bool matches = item.Id == targetId;
+                      if (matches)
+                      {
+                          transientDeleted = true;
+                          transientDeleteResult = new DeleteResult.Acknowledged(1);
+                          return true;
+                      }
+                      return false;
+                  });
+              })
+              .Returns(Task.FromResult(transientDeleteResult));
+
+            var productRepository = new ProductRepository(mockedProductContext?.Object, mockedMapper?.Object);
+
+            //precondition
+            // Assert productRepository does not contain product with id dummy-data
+            var precondition = _productsList.FindAll(x => x.Id == targetId).Count();
+            Assert.Equal(0, precondition);
+
+            bool received = await productRepository.DeleteProduct(targetId);
+
+            // validation
+            Assert.False(received);
+            Assert.Equal(expectedLength, _productsList.Count());
+        }
+
 
         private static List<Product> GetDummyProducts()
         {
