62199b2d423c174a664ef9522b74024e5c494f85
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-07-17 19:45:11 +0530

    Added Az Storage for TF State

--- Documentation/StarterFiles/S7/TfAzure/ResourceGroup/Demo1/main.tf
+++ Documentation/StarterFiles/S7/TfAzure/ResourceGroup/Demo1/main.tf
@@ -6,12 +6,20 @@
 # Terraform/Settings Block
 terraform {
   required_version = ">= 1.2.4"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
   }
+
+  # backend "azurerm" {
+  #   resource_group_name  = "rg-dnlh-12mar-dev"
+  #   storage_account_name = "sttfstatedhls"
+  #   container_name       = "terraformstate"
+  #   key                  = "azrg-terraformstate.tfstate"
+  # }
 }
 
 # Provider Block
